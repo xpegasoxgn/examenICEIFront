d1f2aac1130ef64a00e83deb173743219f78ff0e
PC-VICENTE\PCV <jvicentecv@gmail.com>
2025-07-10 22:07:50 -0400

    listar adopciones component

--- src/app/components/pages/crud_adopcion/listar/listar.component.ts
+++ src/app/components/pages/crud_adopcion/listar/listar.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Adopcion } from '../../../../models/adopcion.model';
+import { AdopcionService } from '../../../../service/adopcion/adopcion.service';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-listar',
@@ -6,6 +10,15 @@ import { Component } from '@angular/core';
   templateUrl: './listar.component.html',
   styleUrl: './listar.component.scss'
 })
-export class ListarComponent {
+export class ListarComponent implements OnInit {
+  adopciones: Adopcion[] = [];
+  displayedColumns: string[] = ['id', 'estado', 'fecha_solicitud', 'fecha_resolucion', 'mascota_id', 'usuario_id'];
 
+  constructor(private adopcionService: AdopcionService) {}
+
+  ngOnInit(): void {
+    this.adopcionService.getAdopciones().subscribe(data => {
+      this.adopciones = data;
+    });
+  }
 }
