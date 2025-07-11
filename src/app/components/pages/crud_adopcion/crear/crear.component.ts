361ccfb6cbcf650c94a5166a3673755ff92c8004
PC-VICENTE\PCV <jvicentecv@gmail.com>
2025-07-10 21:20:46 -0400

    create crear adopciones component

---
+++ src/app/components/pages/crud_adopcion/crear/crear.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crear',
+  standalone: false,
+  templateUrl: './crear.component.html',
+  styleUrl: './crear.component.scss'
+})
+export class CrearComponent {
+
+}
