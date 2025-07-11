536ccaa11b3b7fa07f481b3317ea7cde36bc1eaa
PC-VICENTE\PCV <jvicentecv@gmail.com>
2025-07-10 21:20:26 -0400

    create listar adopciones component

---
+++ src/app/components/pages/crud_adopcion/listar/listar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listar',
+  standalone: false,
+  templateUrl: './listar.component.html',
+  styleUrl: './listar.component.scss'
+})
+export class ListarComponent {
+
+}
