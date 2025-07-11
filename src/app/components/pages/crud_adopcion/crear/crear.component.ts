5ba07d68f58ce96f44b5665c4a9095a7d3e70528
PC-VICENTE\PCV <jvicentecv@gmail.com>
2025-07-10 22:08:05 -0400

    crear adopciones component

--- src/app/components/pages/crud_adopcion/crear/crear.component.ts
+++ src/app/components/pages/crud_adopcion/crear/crear.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Adopcion } from '../../../../models/adopcion.model';
+import { AdopcionService } from '../../../../service/adopcion/adopcion.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-crear',
@@ -7,5 +10,31 @@ import { Component } from '@angular/core';
   styleUrl: './crear.component.scss'
 })
 export class CrearComponent {
+  adopcion: Adopcion = {
+    id: 0,
+    estado: 'PENDIENTE',
+    fecha_resolucion: '',
+    fecha_solicitud: new Date().toISOString().split('T')[0],
+    mascota_id: 0,
+    usuario_id: 0
+  };
+
+  constructor() {}
+
+  guardar() {
+    if (this.adopcion.mascota_id && this.adopcion.usuario_id) {
+      alert('Adopción registrada');
+      this.adopcion = {
+        id: 0,
+        estado: 'PENDIENTE',
+        fecha_resolucion: '',
+        fecha_solicitud: new Date().toISOString().split('T')[0],
+        mascota_id: 0,
+        usuario_id: 0
+      };
+    } else {
+      alert('Completa todos los campos');
+    }
+  }
 
 }
